52ed31a4bd2c56f6c9a6697c56ec3e37b6c00a08
Martin Espadas Jr <bobey90@gmail.com>
2024-11-27 09:19:37 -0800

    Add project files.

---
+++ RVTBootcamp_Module_04/Command1.cs
@@ -0,0 +1,24 @@
+﻿namespace RVTBootcamp_Module_04
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // this is a variable for the Revit application
+            UIApplication uiapp = commandData.Application;
+
+            // this is a variable for the current Revit model
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            // Your code goes here
+            TaskDialog.Show("Test","This is command 1");
+
+
+
+
+            return Result.Succeeded;
+        }
+    }
+
+}
---
+++ RVTBootcamp_Module_04/Command2.cs
@@ -0,0 +1,23 @@
+﻿namespace RVTBootcamp_Module_04
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command2 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // this is a variable for the Revit application
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+
+            // this is a variable for the current Revit model
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            // Your code goes here
+            TaskDialog.Show("Test","This is command 2");
+
+            return Result.Succeeded;
+        }
+
+    }
+
+}
